b713d82cbe888cfdd1acd47d08791c1ede36f6af
Shivang <workshivang@gmail.com>
2026-01-06 23:24:19 +0530

    >>

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from app.api.routes import series, data
 from app.api.v2 import health
 from app.api.v2.routes import router as v2_router
 from app.api.v2.discovery import router as discovery_router
@@ -12,8 +11,6 @@ app = FastAPI(
 )
 
 app.include_router(health.router)
-app.include_router(series.router)
-app.include_router(data.router)
 app.include_router(v2_router)
 app.include_router(discovery_router)
 
